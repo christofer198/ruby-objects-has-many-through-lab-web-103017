e3736d0134128f85a0dc3f37225dd923236109aa
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-11-05 19:33:36 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,22 +1,22 @@
 # class Doctor
-# 
+#
 #   #Variables
 #   attr_accessor :name, :appointments
-# 
+#
 #   def initialize(name)
 #     @name = name
 #     @appointments = []
 #   end
-# 
+#
 #   #Methods
 #   def add_appointment(appointment)
 #     @appointments << appointment
 #   end
-# 
+#
 #   def appointments
 #     @appointments
 #   end
-# 
+#
 #   def patients
 #     @appointments.collect do |appointment|
 #       appointment.patient
