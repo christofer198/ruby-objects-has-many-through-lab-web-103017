5013bbacd24e048aff9a28dcffc416c4786ada05
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-11-05 19:33:28 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,25 +1,45 @@
+# class Doctor
+# 
+#   #Variables
+#   attr_accessor :name, :appointments
+# 
+#   def initialize(name)
+#     @name = name
+#     @appointments = []
+#   end
+# 
+#   #Methods
+#   def add_appointment(appointment)
+#     @appointments << appointment
+#   end
+# 
+#   def appointments
+#     @appointments
+#   end
+# 
+#   def patients
+#     @appointments.collect do |appointment|
+#       appointment.patient
+#     end
+#   end
+# end
 class Doctor
 
-  #Variables
-  attr_accessor :name, :appointments
+attr_reader :name, :appointments
 
   def initialize(name)
     @name = name
     @appointments = []
   end
 
-  #Methods
   def add_appointment(appointment)
     @appointments << appointment
-  end
-
-  def appointments
-    @appointments
+    appointment.doctor = self
   end
 
   def patients
-    @appointments.collect do |appointment|
-      appointment.patient
-    end
+    @appointments.collect{|appointment| appointment.patient}
   end
+
+
 end
